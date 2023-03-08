84fc06e6681597a771040ea7b78a1187afc7d311
Samir Agrawal <agrawasu@mail.uc.edu>
2023-03-08 18:37:35 -0500

    load bricks

--- breakout.js
+++ breakout.js
@@ -25,15 +25,44 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image('ball', 'assets/images/ball.png');
-  this.load.image('paddle', 'assets/images/paddle.png');
-  this.load.image('brick1', 'assets/images/brick1.png');
-  this.load.image('brick2', 'assets/images/brick2.png');
-  this.load.image('brick3', 'assets/images/brick3.png');
+    this.load.image('ball', 'assets/images/ball.png');
+    this.load.image('paddle', 'assets/images/paddle.png');
+    this.load.image('brick1', 'assets/images/brick1.png');
+    this.load.image('brick2', 'assets/images/brick2.png');
+    this.load.image('brick3', 'assets/images/brick3.png');
 }
 function create() {
     player = this.physics.add.sprite(400,600,'paddle');
     ball = this.physics.add.sprite(400,565,'ball');
+
+    redBricks = this.physics.add.group({
+        key: 'brick1',
+        repeat: 9,
+        setXY: {
+            x: 80,
+            y: 140,
+            stepX: 70
+        }
+    });
+    greenBricks = this.physics.add.group({
+        key: 'brick2',
+        repeat: 9,
+        setXY: {
+            x: 80,
+            y: 90,
+            stepX: 70
+        }
+    });
+    blueBricks = this.physics.add.group({
+        key: 'brick3',
+        repeat: 9,
+        setXY: {
+            x: 80,
+            y: 40,
+            stepX: 70
+        }
+    });
+
 }
 function update() {
 
